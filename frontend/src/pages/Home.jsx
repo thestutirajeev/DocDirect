168b5ec1bb1800719ff4e9a4788b130e24ac2fbc
Stuti Rajeev <stutirajeev5@gmail.com>
2025-02-18 14:22:24 +0530

    FAQ in Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,12 +9,14 @@ import icon03 from '../assets/images/icon03.png';
 import featureImg from '../assets/images/feature-img.png';
 import videoIcon from '../assets/images/video-icon.png';
 import avatarIcon from '../assets/images/avatar-icon.png';
+import faqImg from '../assets/images/faq-img.png';
 
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/Doctors/DoctorList';
+import FaqList from '../components/Faq/FaqList';
 const Home = () => {
   return (
     <>
@@ -244,7 +246,19 @@ const Home = () => {
       {/*============= Our Great Doctors=================*/}   
 
       {/*=============FAQ Section==================*/}   
+      <section>
+        <div className='container'>
+          <div className='flex justify-between gap-[50px] lg:gap-0'>
+            <div className='w-1/2 hidden md:block'><img src = {faqImg}  alt="FAQ Img"></img>
+            </div>
 
+            <div className='w-full md:w-1/2'>
+              <h2 className='heading'>Frequently Asked Questions</h2>
+              <FaqList/>
+            </div>
+          </div>
+        </div>
+      </section>
       {/*=============FAQ Section End==================*/}
     </>
 
